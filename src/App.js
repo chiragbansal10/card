f491eac624f7a476368d72fc3f62b3a19c394ab5
Chirag Bansal <chiragban24@gmail.com>
2022-10-04 21:58:50 +0530

    update card

--- src/App.js
+++ src/App.js
@@ -4,32 +4,37 @@ const App=()=>
         <div className="card">
 
             <div className="container">
-                
+            <img src="img_avatar.png"></img>
                <h4><b>Chirag Bansal</b></h4>
 <p>GitHub Username: chiragbansal10</p>
 <p>Current Company: TCS</p>
             </div>
             <div className="container">
+            <img src="img_avatar.png"></img>
                <h4><b>Chirag Bansal</b></h4>
 <p>GitHub Username: chiragbansal10</p>
 <p>Current Company: TCS</p>
             </div>
             <div className="container">
+            <img src="img_avatar.png"></img>
                <h4><b>Chirag Bansal</b></h4>
 <p>GitHub Username: chiragbansal10</p>
 <p>Current Company: TCS</p>
             </div>
             <div className="container">
+            <img src="img_avatar.png"></img>
                <h4><b>Chirag Bansal</b></h4>
 <p>GitHub Username: chiragbansal10</p>
 <p>Current Company: TCS</p>
             </div>
             <div className="container">
+            <img src="img_avatar.png"></img>
                <h4><b>Chirag Bansal</b></h4>
 <p>GitHub Username: chiragbansal10</p>
 <p>Current Company: TCS</p>
             </div>
             <div className="container">
+            <img src="img_avatar.png"></img>
                <h4><b>Chirag Bansal</b></h4>
 <p>GitHub Username: chiragbansal10</p>
 <p>Current Company: TCS</p>
