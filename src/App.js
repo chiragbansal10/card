3ee6f326a72a81e0ce61aec784cdd1fe0e8ef2b8
Chirag Bansal <chiragban24@gmail.com>
2022-10-04 21:47:45 +0530

    card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+const App=()=>
+{
+    return(
+        <div className="card">
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <div className="container">
+                
+               <h4><b>Chirag Bansal</b></h4>
+<p>GitHub Username: chiragbansal10</p>
+<p>Current Company: TCS</p>
+            </div>
+            <div className="container">
+               <h4><b>Chirag Bansal</b></h4>
+<p>GitHub Username: chiragbansal10</p>
+<p>Current Company: TCS</p>
+            </div>
+            <div className="container">
+               <h4><b>Chirag Bansal</b></h4>
+<p>GitHub Username: chiragbansal10</p>
+<p>Current Company: TCS</p>
+            </div>
+            <div className="container">
+               <h4><b>Chirag Bansal</b></h4>
+<p>GitHub Username: chiragbansal10</p>
+<p>Current Company: TCS</p>
+            </div>
+            <div className="container">
+               <h4><b>Chirag Bansal</b></h4>
+<p>GitHub Username: chiragbansal10</p>
+<p>Current Company: TCS</p>
+            </div>
+            <div className="container">
+               <h4><b>Chirag Bansal</b></h4>
+<p>GitHub Username: chiragbansal10</p>
+<p>Current Company: TCS</p>
+            </div>
+        </div>
+    )
 }
-
-export default App;
+export default App
