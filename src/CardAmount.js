0cbc94b78d2aad9c15c349483d7538d0d421d3fb
Chirag Bansal <chiragban24@gmail.com>
2022-10-05 18:54:16 +0530

    card2

--- src/CardAmount.js
+++ src/CardAmount.js
@@ -7,8 +7,24 @@ const CardAmount = () => {
   function AddMoney() {
     setMoney((previousMoney) => previousMoney + 1000);
   }
+  function AddMoneyInput() {
+       
+        let inputVal=document.getElementById("myInput").value
+        setMoney((previousMoney) => 
+        inputVal<previousMoney?
+        previousMoney + parseInt(inputVal):previousMoney
+        )
+  }
   function withdrawMoney() {
     setMoney((previousMoney) => previousMoney - 500);
+  }
+  function withdrawMoneyInput() {
+    let inputVal=document.getElementById("myInput").value
+   
+    setMoney((previousMoney) => 
+    inputVal<previousMoney?
+    previousMoney - inputVal:previousMoney
+    );
   }
   return (
     <div>
@@ -22,9 +38,9 @@ const CardAmount = () => {
       <br />
       <br />
       <div>
-        <input ></input>
-        <button onClick={AddMoney}>Add Money</button>
-        <button onClick={withdrawMoney}>Withdraw Money</button>
+        <input type="number" id="myInput"></input><br/><br/>
+        <button onClick={AddMoneyInput}>Add Money</button>
+        <button onClick={withdrawMoneyInput}>Withdraw Money</button>
       </div>
     </div>
   );
